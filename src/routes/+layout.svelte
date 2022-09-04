54a677edfe81d02d30d8712005c9022be4ad3a06
Gustav Westling <gustav@getsturdy.com>
2022-09-04 12:26:37 +0200

    Mobile fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,16 @@
 <main>
     <div class="flex justify-around bg-gray-900 w-full min-h-screen">
         <div class="max-w-7xl flex-1 text-white">
-            <header class="p-2 font-mono flex items-center gap-4">
+            <header class="p-2 md:px-8 font-mono flex items-center gap-4 flex-col md:flex-row md:justify-between">
+                <div class="inline-flex items-center gap-4">
                 <img src="/laptop-small.png" class="h-8 w-8 md:h-16 md:w-16"/>
                 <span class="md:text-2xl">trendy.dev</span>
+                </div>
 
-                <div class="flex-1"></div>
-
+                <div class="inline-flex items-center gap-4">
                 <a href="/" class:text-red-800={$page.url.pathname === "/"}>Top Starred</a>
                 <a href="/actions/monthly" class:text-red-800={$page.url.pathname === "/actions/monthly"}>Trending</a>
+                </div>
             </header>
 
             <slot/>
