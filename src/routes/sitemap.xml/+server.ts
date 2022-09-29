917c15559071eb5cf5340b3eefc26614dc6d9f30
Gustav Westling <gustav@getsturdy.com>
2022-09-29 11:03:00 +0200

    sitemap: auto generate

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,38 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+import { languages } from '$lib/repos';
+
+export const prerender = true;
+export const csr = false;
+export const ssr = true;
+
+export const GET: RequestHandler = async () => {
+    const url = "https://trendy.dev"
+    const periods = ["day", "week", "month"]
+
+    const body = `<?xml version="1.0" encoding="UTF-8" ?>
+  <urlset
+    xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  >
+    <url>
+      <loc>${url}</loc>
+      <changefreq>always</changefreq>
+      <priority>1.0</priority>
+    </url>
+    ${languages
+        .map(
+            (l) =>  periods.map((period) => `
+    <url>
+      <loc>${url}/${period}/${l.slug}</loc>
+      <changefreq>daily</changefreq>
+      <priority>0.5</priority>
+    </url>
+    `
+        ).join(''))
+        .join('')}
+  </urlset>`
+
+    return new Response(
+        body
+    )
+};
