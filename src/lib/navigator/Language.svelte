05ef6213aa76fbcc7b3a68481e091cd234bce01b
Gustav Westling <gustav@getsturdy.com>
2022-10-04 16:49:01 +0200

    navigator: on hover states

--- src/lib/navigator/Language.svelte
+++ src/lib/navigator/Language.svelte
@@ -5,7 +5,7 @@
 
 <a
 	href="/week/{name}"
-	class="block py-2 px-4 w-18 h-18 inline-flex flex-col justify-between items-center bg-slate-700 text-slate-300 rounded-lg text-center space-y-4"
+	class="block py-2 px-4 w-18 h-18 inline-flex flex-col justify-between items-center bg-slate-700 text-slate-300 rounded-lg text-center space-y-4 hover:text-slate-200 hover:bg-slate-600"
 >
 	<div class="max-h-10 w-full flex-1 inline-flex items-center flex-col justify-center">
 		<img {src} class="max-h-10 " />
